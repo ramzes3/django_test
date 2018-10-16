6204f435a424dd673479092c2dd31a5407a40351
Roman <roman.spesyvtsev@strath.ac.uk>
2018-10-16 14:42:24 +0100

    Added a shot counter to display_shots() Updated get_image() to display multiple subplots equale to the number of equipment present Added Previous and Next shot buttons Included several new check on exceptions including not available folders and shots and not supported file formats

--- plots/views.py
+++ plots/views.py
@@ -12,6 +12,7 @@ import random
 from io import StringIO
 import os, sys
 from datetime import datetime
+from pydoc import locate
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -25,30 +26,135 @@ from .models import Shot_information, Equipment
 
 def getimage(request, shot_id):
         shot = get_object_or_404(Shot_information, pk=shot_id)
+        listOfEquipment = shot.equipment_set.all();
+        #print(shot.equipment_set.all().count())
 #        if request.method=="GET": x = np.arange(0, random.randint(0,3) * np.pi, 0.01)       
 #        else: x = np.arange(0, 2 * np.pi, 0.01)
 #        s = np.cos(x) ** 2
 #        plt.plot(x, s)
-
+        
         #plt.xlabel('xlabel(Z)')
         #plt.ylabel('ylabel(Y)')
         #plt.title('Simple Graph!')
         #plt.grid(True)
-        path = "/media/Arni1/data/2018/20180219/run006/"
-        path = get_path(path,1)
-        print(path)
-        listOfImages = [f for f in os.listdir(path) if f.endswith('.png')]
-        img_path = os.path.join(path,listOfImages[shot.shot_number]);
-        img = mpimg.imread(img_path)
+        plt.figure(1) 
+        for counter, tmp in enumerate(listOfEquipment):
+           print(tmp)
+           #print(tmp.equipment_folder)
+           listOfImages = [f for f in os.listdir(tmp.equipment_folder) if f.endswith('.png')]           
+           ax1 = plt.subplot(3,3,counter+1)             # the first subplot in the first figure 
+           #ax1.set_xticklabels([])
+           #ax1.set_yticklabels([])
+           #ax1.set_aspect('equal') 
+           plt.axis('off')
+           plt.title(tmp.equipment_name)
+           #print(len(listOfImages))
+           if len(listOfImages) < shot.shot_number:
+                ax1.annotate('Shot not present',xy=(0.1,0.5));
+                continue;
+           img_path = os.path.join(tmp.equipment_folder,listOfImages[shot.shot_number]);
+           img = mpimg.imread(img_path)
+
+           plt.imshow(img)
+
+
+        #plt.subplots_adjust(wspace=0, hspace=0)
+
+        #path = "/media/Arni1/data/2018/20180219/run006/"
+        #path = get_path(path,1)
+        #print(path)
+        #listOfImages = [f for f in os.listdir(path) if f.endswith('.png')]
+        #img_path = os.path.join(path,listOfImages[shot.shot_number]);
+        #img = mpimg.imread(img_path)
         #A=np.asarray(img)
-        plt.imshow(img)
+        #plt.imshow(img)
 
 
         response = HttpResponse(content_type="image/png")
-        plt.savefig(response, format="png")
+        plt.savefig(response, format="png",bbox_inches='tight')
 
         return response
 
+def display_shot(request, shot_id):
+        shot = get_object_or_404(Shot_information, pk=shot_id)
+        #if request.method !== 'POST':
+        #     return HttpResponse("There is no data submitted")
+        #r_type = locate(shot.run)
+        if ('Previous' in request.POST) and (shot.shot_number>1):
+             shot.shot_number = shot.shot_number-1
+        elif('Next' in request.POST):
+             shot.shot_number = shot.shot_number+1
+        else:
+             shot.shot_number = int(request.POST['Shot_number'])
+
+        run = int(request.POST['Run'])
+        date = datetime.strptime(request.POST['Date'], "%Y-%m-%d").date()
+        if (shot.run != run) or (shot.aquisition_date != date):
+             status_change=1
+        else:
+             status_change=0
+
+        shot.run = request.POST['Run']
+        shot.aquisition_date = datetime.strptime(request.POST['Date'], "%Y-%m-%d").date()
+        shot_dir = get_shot_dir(request, shot) 
+        
+        if not os.path.isdir(shot_dir):
+             print(shot_dir)
+             return HttpResponse("There is no data available on this day")
+       
+        listOfEquipment = os.listdir(shot_dir)
+  
+        print(listOfEquipment)
+        if status_change:
+             print("Change occured")
+             shot.run = request.POST['Run']
+             shot.aquisition_date = datetime.strptime(request.POST['Date'], "%Y-%m-%d").date()
+             shot.equipment_set.all().delete()
+             for tmp in listOfEquipment:
+                 #print(tmp)  
+                 shot.equipment_set.create(equipment_name=tmp,equipment_folder=os.path.join(shot_dir,tmp))
+
+
+
+
+        #print(shot.aquisition_date)
+        shot.equipment_set.all()
+        shot.save()
+
+        #print(shot.equipment_set.all().count())
+
+
+        
+        context = {'shot': shot, 'c_date':str(shot.aquisition_date),}
+        return render(request, 'plots/display_shot.html',context)
+        #return HttpResponse("Hello, world. You're at the display shots.")
+
+def index(request):
+        path = "/media/Arni1/data/"
+        shot_list = Shot_information.objects.all()    
+        shot = shot_list.filter(user_identifier__startswith='roman')        
+        if shot.count()==0:
+            Shot_information.create(root_folder=path,user_identifier="roman", aquisition_date=timezone.now())
+                     
+
+        shot = get_object_or_404(Shot_information, user_identifier='roman')
+        print( str(shot.aquisition_date) )
+        #shot.run = 1
+        shot.root_folder = path
+        shot.save()   
+        context = {'shot': shot, 'c_date':str(shot.aquisition_date),}
+        #shot_id = get_object_or_404(Shot_information, pk=question_id)
+        return render(request, 'plots/choose_data.html',context)
+
+def get_shot_dir(request, shot):        
+        date_folder = ("".join(map(str,[shot.aquisition_date.year, str(shot.aquisition_date.month).rjust(2,'0'), str(shot.aquisition_date.day).rjust(2,'0')])))
+        run_folder = ("".join(map(str,["run",str(shot.run).rjust(3,'0')])))
+        path = os.path.join(shot.root_folder, str(shot.aquisition_date.year), date_folder,run_folder)
+
+        return path
+
+
+# Redundant code
 def next_image(request, shot_id):
         shot = get_object_or_404(Question, pk=question_id)
         path = "/media/Arni1/data/2018/20180219/run006/"       
@@ -82,56 +188,3 @@ def inter(request, image_id):
 
         image_id =1
         return render(request, 'plots/inter.html',{'image_id': image_id})
-
-def display_shot(request, shot_id):
-        shot = get_object_or_404(Shot_information, pk=shot_id)
-        shot.run = request.POST['Run']
-        shot.aquisition_date = datetime.strptime(request.POST['Date'], "%Y-%m-%d").date()
-        shot_dir = get_shot_dir(request, shot)        
-        print(shot.aquisition_date)
-        print(shot_dir)
-        shot.save()
-
-        print(shot.equipment_set.all().count())
-        shot.equipment_set.all().delete()
-        if not os.path.isdir(shot_dir):
-             return HttpResponse("There is no data available on this day")
-
-        listOfEquipment = os.listdir(shot_dir)
-        
-        for tmp in listOfEquipment:
-             print(tmp)  
-             shot.equipment_set.create(equipment_name=tmp,equipment_folder=os.path.join(shot_dir,tmp))
-             #path = os.path.join(shot_dir,tmp)
-
-        context = {'shot': shot, 'c_date':str(shot.aquisition_date),}
-        return render(request, 'plots/display_shot.html',context)
-        #return HttpResponse("Hello, world. You're at the display shots.")
-
-def index(request):
-        path = "/media/Arni1/data/"
-        shot_list = Shot_information.objects.all()    
-        shot = shot_list.filter(user_identifier__startswith='roman')        
-        if shot.count()==0:
-            Shot_information.create(root_folder=path,user_identifier="roman", aquisition_date=timezone.now())
-                     
-
-        shot = get_object_or_404(Shot_information, user_identifier='roman')
-        print( str(shot.aquisition_date) )
-        #shot.run = 1
-        shot.root_folder = path
-        shot.save()   
-        context = {'shot': shot, 'c_date':str(shot.aquisition_date),}
-        #shot_id = get_object_or_404(Shot_information, pk=question_id)
-        return render(request, 'plots/choose_data.html',context)
-
-def get_shot_dir(request, shot):        
-        date_folder = ("".join(map(str,[shot.aquisition_date.year, str(shot.aquisition_date.month).rjust(2,'0'), str(shot.aquisition_date.day).rjust(2,'0')])))
-        run_folder = ("".join(map(str,["run",str(shot.run).rjust(3,'0')])))
-        path = os.path.join(shot.root_folder, str(shot.aquisition_date.year), date_folder,run_folder)
-
-        return path
-
-
-
-
